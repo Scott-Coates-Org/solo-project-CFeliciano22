21ff31510e04b2e61f9081c6580294450a827e69
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-08-10 01:18:16 -0400

    worked on the home component layed the foundation of ui from the mockup

--- Client/src/components/Home/Home.js
+++ Client/src/components/Home/Home.js
@@ -1,10 +1,16 @@
+import './Home.scss'
+
 const Home = () => {
     return(
         <div className="home">
-            <form>
+            <div className='home-header'>
+            <h2>Simplicity All In One</h2>
+            <p>Send, swap, transactions, and learn all in one. Simplicity at its finest</p>
+            </div>
+            <form className='home-form'>
                 <input type="text" placeholder="eth address"/>
                 <input type="text" placeholder="amount"/>
-                <button>Send</button>
+                <button className='form-button'>Send</button>
             </form>
         </div>
     )
